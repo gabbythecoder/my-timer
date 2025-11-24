5ec74ed04cc12bab70e165643b936cb8e0e18533
gabby <hannahfrench1709@hotmail.com>
2025-11-24 14:37:14 +0000

    feat: add social icons in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
-    return (
-        <footer>
-            <p className="text-sm m-1 p-1">&copy; Gabby French 2025 | Made with ❤️ in the UK</p>
-        </footer>
-    )
+  return (
+    <footer className="flex justify-between items-center m-1">
+      <p className="text-xs tracking-wide m-1 p-1">
+        &copy; Gabby French 2025 | Made with ❤️ in the UK
+      </p>
+
+      <div className="flex gap-2 m-1 p-1 text-[var(--foreground)]">
+        <a
+          href="https://github.com/gabbythecoder"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Github"
+        >
+          <FaGithub size={24} />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/gabbyy-frenchh/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Linkedin"
+        >
+          <FaLinkedin size={24} />
+        </a>
+      </div>
+    </footer>
+  );
 }
